6f79a0d3a7c518dde44bb293f6f9fea67fd53a06
Florian Barbare <florian.barbare@plentific.com>
2024-01-10 15:33:06 +0000

    Fix await

--- index.js
+++ index.js
@@ -22,60 +22,65 @@ function findEmailCommitAPI(apiData) {
   }
 }
 
-try {
-  //inputs defined in action metadata file
-  const usernameForEmail = process.env.USERNAME;
-  const token = process.env.TOKEN;
-
-  console.log(`[*] Getting ${usernameForEmail}\'s GitHub email`);
-
-  //attempt to use auth token to get email via accessing the user's API page
-  let userAPIData = null;
+async function run() {
   try {
-    const octokit = new Octokit({ auth: `${token}` });
-    userAPIData = await octokit.request(`GET /users/${usernameForEmail}`, {});
+    //inputs defined in action metadata file
+    const usernameForEmail = process.env.USERNAME;
+    const token = process.env.TOKEN;
+
+    console.log(`[*] Getting ${usernameForEmail}\'s GitHub email`);
+
+    //attempt to use auth token to get email via accessing the user's API page
+    let userAPIData = null;
+    try {
+      const octokit = new Octokit({ auth: `${token}` });
+      userAPIData = await octokit.request(`GET /users/${usernameForEmail}`, {});
+    } catch (error) {
+      console.log('[!] ' + error.message);
+    }
+
+    // Extract the email if the user's API was accessed successfully
+    let emailUserpage = null;
+    if (
+      userAPIData != null &&
+      userAPIData.data != null &&
+      userAPIData.data.email != null &&
+      userAPIData.data.email != ''
+    ) {
+      emailUserpage = userAPIData.data.email;
+    }
+
+    //email not found on user's API page or failed to authenticate with token, fallback to old method to attempt email retrieval
+    if (emailUserpage == null) {
+      console.log(`[*] Falling back to old API retrieval method`);
+
+      //fetch user's public events page
+      fetch(`https://api.github.com/users/${usernameForEmail}/events/public`)
+        .then(function (response) {
+          // When the page is loaded convert it to text
+          return response.text();
+        })
+        .then(apiData => {
+          const emailEventsPage = findEmailCommitAPI(apiData);
+
+          if (emailEventsPage == null) {
+            throw Error('[!!!] Could not find email in API Data');
+          }
+
+          console.log(`[*] Found ${usernameForEmail}\'s email: ${emailEventsPage}`);
+          core.setOutput('email', emailEventsPage);
+        })
+        .catch(error => {
+          core.setFailed(error.message);
+        });
+    } else {
+      console.log(`[*] Found ${usernameForEmail}\'s email: ${emailUserpage}`);
+      core.setOutput('email', emailUserpage);
+    }
   } catch (error) {
-    console.log('[!] ' + error.message);
+    core.setFailed(error.message);
+    throw error;
   }
-
-  // Extract the email if the user's API was accessed successfully
-  let emailUserpage = null;
-  if (
-    userAPIData != null &&
-    userAPIData.data != null &&
-    userAPIData.data.email != null &&
-    userAPIData.data.email != ''
-  ) {
-    emailUserpage = userAPIData.data.email;
-  }
-
-  //email not found on user's API page or failed to authenticate with token, fallback to old method to attempt email retrieval
-  if (emailUserpage == null) {
-    console.log(`[*] Falling back to old API retrieval method`);
-
-    //fetch user's public events page
-    fetch(`https://api.github.com/users/${usernameForEmail}/events/public`)
-      .then(function (response) {
-        // When the page is loaded convert it to text
-        return response.text();
-      })
-      .then(apiData => {
-        const emailEventsPage = findEmailCommitAPI(apiData);
-
-        if (emailEventsPage == null) {
-          throw Error('[!!!] Could not find email in API Data');
-        }
-
-        console.log(`[*] Found ${usernameForEmail}\'s email: ${emailEventsPage}`);
-        core.setOutput('email', emailEventsPage);
-      })
-      .catch(error => {
-        core.setFailed(error.message);
-      });
-  } else {
-    console.log(`[*] Found ${usernameForEmail}\'s email: ${emailUserpage}`);
-    core.setOutput('email', emailUserpage);
-  }
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+run.catch(console.error);
